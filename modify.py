21251954b353b639ef7f88afe80df9ac6d7c7fdf
Longjun Li <2674858716@qq.com>
2018-12-07 11:09:59 -0600

    Add files via upload

---
+++ modify.py
@@ -0,0 +1,14 @@
+import os
+
+file = open('abstract.json','r')
+content = file.read()
+file.close()
+os.remove("abstract.json")
+
+fixed_content = content[11:-2]
+fixed_content = fixed_content.replace('},{','}\n{')
+#for line in fixed_content:
+#	print line
+#	x = raw_input("test")
+fixed_file = open('modified_abstract.json','w')
+fixed_file.write(fixed_content)
